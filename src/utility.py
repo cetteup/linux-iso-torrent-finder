1537f31ef0423f7c2e6d70ff31acf8b94b5b7a11
cetteup <me@cetteup.com>
2021-11-05 09:33:06 +0100

    Include .ova.torrent files (Parrot)

--- src/utility.py
+++ src/utility.py
@@ -26,7 +26,7 @@ async def find_torrents_recursively(root_url: str) -> List[str]:
     # Check for torrent files on current level
     torrents_file_urls = list(set([urljoin(root_url, link.get('href')) for link in
                                    soup.select('a[href$=".iso.torrent"], a[href$=".img.torrent"], '
-                                               'a[href$=".zip.torrent"]')]))
+                                               'a[href$=".zip.torrent"], a[href$=".ova.torrent"]')]))
     logging.debug(f'Found {len(torrents_file_urls)} unique image torrents in current directory')
 
     # Check for torrent files on sub-levels
